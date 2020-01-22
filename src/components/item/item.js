14f692d10752cd4089da6e9062a8dd4784d214af
vinod-wal <vinod.v@westagilelabs.com>
2020-01-22 15:46:25 +0530

    Add CTAs to item

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -4,13 +4,18 @@ import React from 'react';
 const Item = (props) => {
     return (
         <div style={styles.container}>
-            <input type="checkbox" style={styles.checkbox}/>
+            <input type="checkbox" />
             <div style={styles.centerContent}>
                 <p style={styles.title}> {props.task.title} </p>
                 <p style={styles.authorName}>
                     <span style={styles.authorNameLabel}>author : </span> 
                     {props.task.author.name} 
                 </p>
+            </div>
+            <div style={styles.buttonsContainer}>
+                <button style={styles.cta}>&#x270E;</button>
+                <div style={styles.spacer}></div>
+                <button style={styles.cta}>&#x1F5D1;</button>
             </div>
         </div>
     )
@@ -25,9 +30,6 @@ const styles = {
         padding: '12px',
         maxWidth: '500px',
     },
-    // checkbox: {
-    //     padding: '15px',
-    // },
     centerContent: {
         padding: '0 5px',
     },
@@ -35,6 +37,7 @@ const styles = {
         margin: 0,
         fontSize: '14px',
         paddingBottom: '5px',
+        textOverflow: 'wrap',
     },
     authorName: {
         margin: 0,
@@ -45,8 +48,18 @@ const styles = {
         fontSize: '9px',
     },
     buttonsContainer: {
-
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: 'auto',
+    },
+    cta: {
+        border: '0px solid transparent',
+        fontSize: '14px',
+    },
+    spacer: {
+        height: '5px',
     }
+
 }
 
 export default Item;
